18960e852ceded3735650c35f3e1aadbea8c5fb9
TamirAtia <42617092+TamirAtia@users.noreply.github.com>
2022-03-27 18:57:32 +0300

    Delete DbBash.sh

--- DbBash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-
-apt install docker.io -y
-
-docker pull postgres:latest
-
-docker run --restart always -d --name measurements -p 5432:5432 -e 'POSTGRES_PASSWORD=p@ssw0rd42' postgres
